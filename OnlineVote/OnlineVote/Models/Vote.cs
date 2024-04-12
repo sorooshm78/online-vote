dc60fd20756b93196b76810b0f8765feb90ffa1c
Soroush mohammadi <sorooshm78@gmail.com>
2024-04-12 20:22:23 +0330

    intial db

--- OnlineVote/OnlineVote/Models/Vote.cs
+++ OnlineVote/OnlineVote/Models/Vote.cs
@@ -5,8 +5,7 @@
         public int Id { get; set; }
         public string IP {  get; set; }
         public DateTime CreatedTime { get; set; }
-
-        public Question Question { get; set; }
+       
         public List<Option> Options { get; set; }   
     }
 }
