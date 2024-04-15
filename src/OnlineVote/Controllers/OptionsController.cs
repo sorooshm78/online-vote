9de0570033210fd2cd9d2087eb642d25285c42a8
Soroush mohammadi <sorooshm78@gmail.com>
2024-04-15 17:52:19 +0330

    change structure

---
+++ src/OnlineVote/Controllers/OptionsController.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using OnlineVote.Data;
+using OnlineVote.Models;
+
+namespace OnlineVote.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OptionsController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+
+        public OptionsController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Options
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Option>>> GetOptions()
+        {
+            return await _context.Options.ToListAsync();
+        }
+
+        // GET: api/Options/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Option>> GetOption(int id)
+        {
+            var option = await _context.Options.FindAsync(id);
+
+            if (option == null)
+            {
+                return NotFound();
+            }
+
+            return option;
+        }
+
+        // PUT: api/Options/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutOption(int id, Option option)
+        {
+            if (id != option.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(option).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!OptionExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Options
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<Option>> PostOption(Option option)
+        {
+            _context.Options.Add(option);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetOption", new { id = option.Id }, option);
+        }
+
+        // DELETE: api/Options/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteOption(int id)
+        {
+            var option = await _context.Options.FindAsync(id);
+            if (option == null)
+            {
+                return NotFound();
+            }
+
+            _context.Options.Remove(option);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool OptionExists(int id)
+        {
+            return _context.Options.Any(e => e.Id == id);
+        }
+    }
+}
