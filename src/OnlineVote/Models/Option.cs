0f4e3f9f8f16911183888dbb303df2bffbffd500
Soroush mohammadi <sorooshm78@gmail.com>
2024-04-19 17:00:03 +0330

    remove empty line

--- src/OnlineVote/Models/Option.cs
+++ src/OnlineVote/Models/Option.cs
@@ -4,10 +4,8 @@
     {
         public int Id { get; set; }
         public string? Title { get; set; }
-        
         public int QuestionId {  get; set; }
         public Question Question { get; set; }
-
         public List<Vote> Votes { get; set; }
     }
 }
